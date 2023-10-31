6f82cb56f2a060e3a0c6c0545664730b898c7254
kevins00 <daxinshen@gmail.com>
2023-10-30 20:15:38 -0700

    now only enters pinball on collision with boss alone

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -706,7 +706,7 @@ bool WorldSystem::step_world(float elapsed_ms_since_last_update)
 
 	generate_enemy_timer += elapsed_ms_since_last_update / 1000.0f; // Convert elapsed time to seconds
 
-    if (generate_enemy_timer >= 2.f) {
+    if (generate_enemy_timer >= 5.f) {
 		vec2 pos = registry.motions.get(registry.rooms.entities[0]).position;
 		Entity ene = createRoomEnemy(renderer, { pos[0]+distribution1(gen), pos[1]+distribution1(gen) }, pos, 700.f, false);
 		registry.colors.insert(ene, { distribution2(gen), distribution2(gen), distribution2(gen) });
@@ -730,7 +730,7 @@ void WorldSystem::handle_collisions_world()
 		if (registry.players.has(entity))
 		{
 			// Checking Player - Enemy collisions
-			if (registry.mainWorldEnemies.has(entity_other))
+			if (registry.mainWorldEnemies.has(entity_other) && registry.positionKeyFrames.has(entity_other) && registry.mainWorldEnemies.size() == 1)
 			{
 				if (!registry.enterCombatTimer.has(entity))
 				{
@@ -763,8 +763,22 @@ void WorldSystem::handle_collisions_world()
 		if (registry.mainWorldEnemies.has(entity) && registry.playerBullets.has(entity_other) ||
 			registry.mainWorldEnemies.has(entity_other) && registry.playerBullets.has(entity)) {
 			// remove enemy upon collision
-			registry.remove_all_components_of(entity);
-			registry.remove_all_components_of(entity_other);
+			if (!registry.positionKeyFrames.has(entity) && !registry.positionKeyFrames.has(entity_other)) {
+				registry.remove_all_components_of(entity);
+				registry.remove_all_components_of(entity_other);
+			}
+		}
+
+		// enemyy bullet vs player collision
+		if (registry.enemyBullets.has(entity) && registry.players.has(entity_other) ||
+			registry.enemyBullets.has(entity_other) && registry.players.has(entity)) {
+			// handle damage interaction (nothing for now)
+			if (registry.enemyBullets.has(entity)) {
+				registry.remove_all_components_of(entity);
+			}
+			else {
+				registry.remove_all_components_of(entity_other);
+			}
 		}
 	}
 
