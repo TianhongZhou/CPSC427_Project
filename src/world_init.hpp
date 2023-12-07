2418ea5292057883154ebd7bfbe03344edeaaf94
kevins00 <daxinshen@gmail.com>
2023-12-07 14:03:50 -0800

    changed starting room to have more drops, changed pinball combat to reduce hp instead of restarting game

--- src/world_init.hpp
+++ src/world_init.hpp
@@ -51,6 +51,7 @@ Entity createSpikes(vec2 pos, vec2 size);
 Entity createBar(RenderSystem* renderer, vec2 pos, vec2 scale);
 
 Entity createStartingRoom(RenderSystem* renderer, vec2 pos, GLFWwindow* window);
+Entity createMaze(RenderSystem* renderer, vec2 pos, GLFWwindow* window);
 Entity createMaze1(RenderSystem* renderer, vec2 pos);
 Entity createRoom1(RenderSystem* renderer, vec2 pos);
 Entity createRoom2(RenderSystem* renderer, vec2 pos);
